0abf1a56a75129b450d2cedff0368c7c0f3d045a
Subham Shrestha <subham.hero01@gmail.com>
2022-10-17 13:14:10 +0545

    example added

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -1,20 +1,43 @@
 import { createElement } from "react";
 import { Menu, MenuProps, useResourceDefinitions } from "react-admin";
+import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { ListItem } from "@mui/material";
+import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
 
 const dropDownMenus = ["fuel-cards"];
 
 export const CustomMenu = (props: MenuProps) => {
   const resources = useResourceDefinitions();
+  const navigate = useNavigate();
+
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   const createMenuItem = (name: string) => {
     return (
-      <Menu.Item
-        key={name}
-        to={`/${name}`}
-        primaryText={
-          (resources[name].options && resources[name].options.label) || name
-        }
-        leftIcon={createElement(resources[name].icon)}
-      />
+      <ListItemButton
+        onClick={(e) => {
+          navigate(`/${name}`);
+        }}
+      >
+        <ListItemIcon>{createElement(resources[name].icon)}</ListItemIcon>
+        <ListItemText
+          primary={
+            (resources[name].options && resources[name].options.label) || name
+          }
+        />
+      </ListItemButton>
     );
   };
 
@@ -24,9 +47,23 @@ export const CustomMenu = (props: MenuProps) => {
 
   return (
     <Menu {...props}>
-      {simpleMenus.map((name) => {
-        return createMenuItem(name);
-      })}
+      <List>
+        {simpleMenus.map((name) => {
+          return createMenuItem(name);
+        })}
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <LocalGasStationIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dropdown" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List disablePadding>
+            <ListItem sx={{ pl: 4 }}>{createMenuItem("fuel-cards")}</ListItem>
+          </List>
+        </Collapse>
+      </List>
     </Menu>
   );
 };
--- src/pages/fuel-cards/create.tsx
+++ src/pages/fuel-cards/create.tsx
@@ -1,10 +1,24 @@
-import { Create, ReferenceInput, SimpleForm, TextInput } from "react-admin";
+import {
+  AutocompleteArrayInput,
+  Create,
+  ReferenceInput,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
 
 export const FuelCardCreate = () => (
   <Create>
     <SimpleForm>
       <TextInput source="location" />
       <ReferenceInput source="vehicleId" reference="vehicles" />
+      <AutocompleteArrayInput
+        source="tags"
+        choices={[
+          { id: "programming", name: "Programming" },
+          { id: "lifestyle", name: "Lifestyle" },
+          { id: "photography", name: "Photography" },
+        ]}
+      />
     </SimpleForm>
   </Create>
 );
